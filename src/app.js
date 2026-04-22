0e84dafa548bc99e68ab119ad32eb9ee2421c12e
SEbstiaan <sebasbernal0719@gmail.com>
2026-04-22 15:52:53 -0500

    a

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('button');
+const formulario = document.getElementById('formulario');
+
+
+
+formulario.addEventListener('submit', function() {
+    alert('Your message has been sent!');
+});
