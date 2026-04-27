904d11c49aabe729d97bba8915eabdc1b3efe5cd
Sebastian Bernal <sebasbernal0719@gmail.com>
2026-04-26 23:05:49 -0500

    refactor: Mejore la parte del JS y minifiqué el CSS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const button = document.getElementById('button');
 const formulario = document.getElementById('formulario');
+const button2 = document.getElementById('clickme-button');
 
-
-
-formulario.addEventListener('submit', function() {
-    alert('Your information has been sent!');
+window.addEventListener('load', function() {
+    alert('Welcome to my portafolio User!');
 });
+
+button2.addEventListener('click', function() {
+    if (button2.textContent === 'You clicked the button!') {
+        button2.textContent = 'Click me!';
+    }
+    else{
+        button2.textContent = 'You clicked the button!';
+    }
+
+});
+
